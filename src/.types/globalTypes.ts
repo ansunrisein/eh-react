7cd8bfe93ba8c9d4cd448f0ab62b045d0840eca5
heiNina <nina.shabrova2001@gmail.com>
2020-11-29 17:40:31 +0300

    Add graphql tools and schema

---
+++ src/.types/globalTypes.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum EventType {
+  LIST = 'LIST',
+  PICTURE = 'PICTURE',
+  TEXT = 'TEXT',
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
---
+++ src/features/horizon/graphql/types/EventFragment.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import {EventType} from './../../../../.types/globalTypes'
+
+// ====================================================
+// GraphQL fragment: EventFragment
+// ====================================================
+
+export interface EventFragment_TextEvent {
+  type: EventType
+  header: string | null
+  deadline: any | null
+  text: string
+}
+
+export interface EventFragment_ListEvent {
+  type: EventType
+  header: string | null
+  deadline: any | null
+  list: string[]
+}
+
+export type EventFragment = EventFragment_TextEvent | EventFragment_ListEvent
