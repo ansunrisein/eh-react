8fab4819edf336ddeae032d7a636608c6fa4662c
ansunrisein <ansunrisein@gmail.com>
2021-02-28 23:25:22 +0300

    Add create board button

--- src/features/horizon/views/Dashboard/Dashboard.tsx
+++ src/features/horizon/views/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Box, Flex} from 'reflexbox'
+import {Icon, IconButton} from 'rsuite'
 import {
   BoardList,
   BoardListProps,
@@ -24,18 +25,21 @@ export const Dashboard: React.FC<DashboardProps> = ({
   onFiltersChange,
   onSortsChange,
   boards,
-}) => {
-  return (
-    <Flex height="100%" overflow="hidden">
+}) => (
+  <Flex height="100%" overflow="hidden">
+    <Flex flexDirection="column" justifyContent="space-between">
       <Flex flexDirection="column">
         <Filters filters={filters} onChange={onFiltersChange} />
         <Spacing space="0.5rem" vertical />
         <Sorts sorts={sorts} onChange={onSortsChange} />
       </Flex>
-      <Spacing space="0.5rem" />
-      <Box flex="1" height="100%" overflow="hidden auto">
-        <BoardList boards={boards} />
+      <Box>
+        <IconButton icon={<Icon icon="plus" />} size="lg" />
       </Box>
     </Flex>
-  )
-}
+    <Spacing space="0.5rem" />
+    <Box flex="1" height="100%" overflow="hidden auto">
+      <BoardList boards={boards} />
+    </Box>
+  </Flex>
+)
