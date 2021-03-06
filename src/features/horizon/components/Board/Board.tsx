d356a4df547868c9aaad7e5ff8ee94de73083c5f
ansunrisein <ansunrisein@gmail.com>
2021-03-06 23:49:31 +0300

    Add BoardControl to Board

--- src/features/horizon/components/Board/Board.tsx
+++ src/features/horizon/components/Board/Board.tsx
@@ -1,7 +1,8 @@
 import React, {useCallback, useState} from 'react'
-import {Button, Icon, Panel} from 'rsuite'
+import {Button, Divider, Icon, Panel} from 'rsuite'
 import {Box, Flex} from 'reflexbox'
 import {BoardFragment} from '../../graphql/types/BoardFragment'
+import {BoardControl} from '../BoardControl'
 import {EventGrid} from '../EventGrid'
 import {EventLine} from '../EventLine'
 
@@ -29,9 +30,13 @@ export const Board: React.FC<BoardProps> = ({board, expandable, onExpand, onColl
 
   return (
     <Panel bordered shaded style={{position: 'relative'}}>
-      <Flex justifyContent="space-between" marginBottom="1rem">
-        <h5>{board.name}</h5>
-        <span>{board.description}</span>
+      <Flex marginBottom="1rem" justifyContent="space-between" alignItems="center">
+        <Flex alignItems="flex-end">
+          <h5>{board.name}</h5>
+          <Divider vertical style={{height: 'auto', alignSelf: 'stretch'}} />
+          <span>{board.description}</span>
+        </Flex>
+        <BoardControl />
       </Flex>
       {expanded ? <EventGrid events={board.events} /> : <EventLine events={board.events} />}
       {expandable && (
