50bf30f1508ebf65aa3975c540ac0f3d655bf395
ansunrisein <ansunrisein@gmail.com>
2021-03-07 18:39:30 +0300

    Implement Dashboard displays switching

--- src/features/horizon/views/Dashboard/Dashboard.test.tsx
+++ src/features/horizon/views/Dashboard/Dashboard.test.tsx
@@ -4,6 +4,8 @@ import {Dashboard} from './Dashboard'
 import {boards, filters, sorts} from './testData'
 
 describe('Dashboard', () => {
+  const displays = ['list', 'grid'] as const
+
   it('should call onFiltersChange', () => {
     const onChange = jest.fn()
     render(<Dashboard filters={filters} sorts={sorts} boards={boards} onFiltersChange={onChange} />)
@@ -16,5 +18,34 @@ describe('Dashboard', () => {
     render(<Dashboard filters={filters} sorts={sorts} boards={boards} onSortsChange={onChange} />)
     act(() => screen.getAllByTestId('sort-button')[0].click())
     expect(onChange).toBeCalledTimes(1)
+  })
+
+  it('should set default display to list', () => {
+    render(<Dashboard boards={boards} filters={filters} sorts={sorts} />)
+    const boardList = screen.getByTestId('board-list')
+    expect(boardList).toBeInTheDocument()
+  })
+
+  test.each(displays)('should set default display', display => {
+    render(<Dashboard boards={boards} filters={filters} sorts={sorts} defaultDisplay={display} />)
+    const boardDisplay = screen.getByTestId(`board-${display}`)
+    expect(boardDisplay).toBeInTheDocument()
+  })
+
+  test.each(displays)('should render only one display', display => {
+    const restDisplays = displays.filter(e => e !== display)
+    render(<Dashboard boards={boards} filters={filters} sorts={sorts} defaultDisplay={display} />)
+    for (const display of restDisplays) {
+      const board = document.querySelector(`*[data-testid=board-${display}]`)
+      expect(board).toBeNull()
+    }
+  })
+
+  test.each(displays)('should switch display on click', display => {
+    render(<Dashboard boards={boards} filters={filters} sorts={sorts} defaultDisplay={display} />)
+    act(() => screen.getByTestId('switch-display').click())
+    const next = displays[(displays.indexOf(display) + 1) % displays.length]
+    const boardDisplay = screen.getByTestId(`board-${next}`)
+    expect(boardDisplay).toBeInTheDocument()
   })
 })
--- src/features/horizon/views/Dashboard/Dashboard.tsx
+++ src/features/horizon/views/Dashboard/Dashboard.tsx
@@ -1,38 +1,61 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {Box, Flex} from 'reflexbox'
 import {Icon, IconButton} from 'rsuite'
 import {Spacing} from '@eh/react/ui'
-import {BoardList, BoardListProps, Filters, FiltersProps, Sorts, SortsProps} from '../../components'
+import {Dashboard_dashboard_edges} from '../../graphql/types/Dashboard'
+import {BoardGrid, BoardList, Filters, FiltersProps, Sorts, SortsProps} from '../../components'
 
 export type DashboardProps = {
+  boards: Dashboard_dashboard_edges[]
   filters: FiltersProps['filters']
   sorts: SortsProps['sorts']
   onFiltersChange?: FiltersProps['onChange']
   onSortsChange?: SortsProps['onChange']
-  boards: BoardListProps['boards']
+  defaultDisplay?: 'list' | 'grid'
 }
 
 export const Dashboard: React.FC<DashboardProps> = ({
+  boards,
   filters,
   sorts,
   onFiltersChange,
   onSortsChange,
-  boards,
-}) => (
-  <Flex height="100%" overflow="hidden">
-    <Flex flexDirection="column" justifyContent="space-between">
-      <Flex flexDirection="column">
-        <Filters filters={filters} onChange={onFiltersChange} />
-        <Spacing space="0.5rem" vertical />
-        <Sorts sorts={sorts} onChange={onSortsChange} />
+  defaultDisplay = 'list',
+}) => {
+  const [display, setDisplay] = useState(defaultDisplay)
+
+  const switchDisplay = useCallback(
+    () => setDisplay(display => (display === 'list' ? 'grid' : 'list')),
+    [],
+  )
+
+  return (
+    <Flex height="100%" overflow="hidden">
+      <Flex flexDirection="column" justifyContent="space-between">
+        <Flex flexDirection="column">
+          <Filters filters={filters} onChange={onFiltersChange} />
+          <Spacing space="0.5rem" vertical />
+          <Sorts sorts={sorts} onChange={onSortsChange} />
+        </Flex>
+        <Box>
+          <IconButton
+            icon={<Icon icon={display === 'grid' ? 'align-justify' : 'th'} />}
+            size="lg"
+            onClick={switchDisplay}
+            data-testid="switch-display"
+          />
+          <Spacing space="1rem" vertical />
+          <IconButton icon={<Icon icon="plus" />} size="lg" />
+        </Box>
       </Flex>
-      <Box>
-        <IconButton icon={<Icon icon="plus" />} size="lg" />
+      <Spacing space="0.5rem" />
+      <Box flex="1" height="100%" overflow="hidden auto">
+        {display === 'list' ? (
+          <BoardList boards={boards} />
+        ) : display === 'grid' ? (
+          <BoardGrid boards={boards} />
+        ) : null}
       </Box>
     </Flex>
-    <Spacing space="0.5rem" />
-    <Box flex="1" height="100%" overflow="hidden auto">
-      <BoardList boards={boards} />
-    </Box>
-  </Flex>
-)
+  )
+}
