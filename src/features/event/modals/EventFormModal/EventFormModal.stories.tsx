a6574fbf58eff804ffe809695e42325b98e2d752
ansunrisein <ansunrisein@gmail.com>
2021-03-11 01:26:03 +0300

    Add modals stories

--- src/features/event/modals/EventFormModal/EventFormModal.stories.tsx
+++ src/features/event/modals/EventFormModal/EventFormModal.stories.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import {Meta, Story} from '@storybook/react'
-import {EventFormModal} from './EventFormModal'
+import {MockedProvider} from '@apollo/client/testing'
+import {EventFormModal, EventFormModalProps} from './EventFormModal'
 
 export default {
   component: EventFormModal,
   title: 'EventFormModal',
+  decorators: [
+    Story => (
+      <MockedProvider mocks={[]}>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+  argTypes: {
+    onHide: {
+      table: {
+        disable: true,
+      },
+    },
+    boardId: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    show: true,
+  },
 } as Meta
 
-export const Usual: Story = () => <EventFormModal />
+export const Usual: Story<EventFormModalProps> = props => <EventFormModal {...props} />
--- src/features/horizon/modals/BoardFormModal/BoardFormModal.stories.tsx
+++ src/features/horizon/modals/BoardFormModal/BoardFormModal.stories.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import {Meta, Story} from '@storybook/react'
-import {BoardFormModal} from './BoardFormModal'
+import {MockedProvider} from '@apollo/client/testing'
+import {BoardFormModal, BoardFormModalProps} from './BoardFormModal'
 
 export default {
   component: BoardFormModal,
   title: 'BoardFormModal',
+  decorators: [
+    Story => (
+      <MockedProvider mocks={[]}>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+  argTypes: {
+    onHide: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    show: true,
+  },
 } as Meta
 
-export const Usual: Story = () => <BoardFormModal />
+export const Usual: Story<BoardFormModalProps> = props => <BoardFormModal {...props} />
