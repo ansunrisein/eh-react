cbbe6b0d239a91405565608eb9eee90bc5beedd5
ansunrisein <ansunrisein@gmail.com>
2021-04-05 13:08:50 +0300

    Pass Header props

--- src/features/common/components/Header/Header.stories.tsx
+++ src/features/common/components/Header/Header.stories.tsx
@@ -5,7 +5,14 @@ import {Header} from './Header'
 export default {
   component: Header,
   title: 'common/components/Header',
-  parameters: {controls: {hideNoControlsWarning: true}},
+  parameters: {layout: 'fullscreen', controls: {hideNoControlsWarning: true}},
+  argTypes: {
+    onDashboardClick: {table: {disable: true}},
+    onLoginClick: {table: {disable: true}},
+    onNotificationsClick: {table: {disable: true}},
+    onLogOutClick: {table: {disable: true}},
+    onProfileClick: {table: {disable: true}},
+  },
 } as Meta
 
-export const Usual: Story = () => <Header />
+export const Usual: Story = props => <Header {...props} />
--- src/features/common/components/Header/Header.tsx
+++ src/features/common/components/Header/Header.tsx
@@ -1,25 +1,66 @@
 import React from 'react'
 import {Flex} from 'reflexbox'
-import {Icon, Popover, Whisper} from 'rsuite'
+import {Button, Icon, Popover, Whisper} from 'rsuite'
 import {Logo} from '@eh/react/ui'
-import {UserMenu} from '../UserMenu'
+import {UserMenu, UserMenuProps} from '../UserMenu'
 
-export const Header: React.FC = () => (
+export type HeaderProps = {
+  isAuthenticated?: boolean
+  onDashboardClick?: () => unknown
+  onLoginClick?: () => unknown
+} & Pick<
+  UserMenuProps,
+  'notificationsCount' | 'onNotificationsClick' | 'onLogOutClick' | 'onProfileClick'
+>
+
+export const Header: React.FC<HeaderProps> = ({
+  isAuthenticated,
+  onDashboardClick,
+  onLoginClick,
+  notificationsCount,
+  onNotificationsClick,
+  onLogOutClick,
+  onProfileClick,
+}) => (
   <Flex as="header" justifyContent="space-between" alignItems="center" padding="0.5rem">
-    <div>
-      <Icon icon="dashboard" size="2x" />
-    </div>
-    <Logo />
-    <Whisper
-      placement="bottomEnd"
-      trigger="click"
-      speaker={
-        <Popover full>
-          <UserMenu style={{width: '10rem'}} />
-        </Popover>
-      }
+    <Button
+      onClick={onDashboardClick}
+      appearance="link"
+      style={{color: 'inherit'}}
+      aria-label="dashboard"
     >
-      <Icon icon="user" size="2x" />
-    </Whisper>
+      <Icon icon="dashboard" size="2x" />
+    </Button>
+    <Logo />
+    {isAuthenticated ? (
+      <Whisper
+        placement="bottomEnd"
+        trigger="click"
+        speaker={
+          <Popover full>
+            <UserMenu
+              style={{width: '10rem'}}
+              notificationsCount={notificationsCount}
+              onNotificationsClick={onNotificationsClick}
+              onLogOutClick={onLogOutClick}
+              onProfileClick={onProfileClick}
+            />
+          </Popover>
+        }
+      >
+        <Button appearance="link" style={{color: 'inherit'}} aria-label="me">
+          <Icon icon="user" size="2x" />
+        </Button>
+      </Whisper>
+    ) : (
+      <Button
+        onClick={onLoginClick}
+        appearance="link"
+        style={{color: 'inherit'}}
+        aria-label="login"
+      >
+        <Icon icon="sign-in" size="2x" />
+      </Button>
+    )}
   </Flex>
 )
