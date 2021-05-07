1e46f1667bed5380d989bb516444207c89c347dd
ansunrisein <ansunrisein@gmail.com>
2021-05-07 20:06:50 +0300

    Implement updateBoard

--- src/features/horizon/graphql/mutations.ts
+++ src/features/horizon/graphql/mutations.ts
@@ -2,8 +2,31 @@ import {gql} from '@apollo/client'
 import {BOARD_FRAGMENT} from './fragments'
 
 export const CREATE_BOARD = gql`
-  mutation CreateBoard($title: String!, $description: String) {
-    createBoard(title: $title, description: $description) {
+  mutation CreateBoard($title: String!, $description: String, $private: Boolean!) {
+    createBoard(title: $title, description: $description, private: $private) {
+      ...BoardFragment
+    }
+  }
+  ${BOARD_FRAGMENT}
+`
+
+export const UPDATE_BOARD = gql`
+  mutation UpdateBoard(
+    $_id: ID!
+    $title: String!
+    $description: String
+    $private: Boolean!
+    $favorite: Boolean!
+    $pinned: Boolean!
+  ) {
+    updateBoard(
+      _id: $_id
+      title: $title
+      description: $description
+      private: $private
+      favorite: $favorite
+      pinned: $pinned
+    ) {
       ...BoardFragment
     }
   }
