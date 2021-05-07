ed247fbfbf89ac573d88766e09ccbb3f2c78b13d
ansunrisein <ansunrisein@gmail.com>
2021-05-07 20:32:29 +0300

    Configure apollo

---
+++ src/configs/apollo.ts
@@ -0,0 +1,31 @@
+import {ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject} from '@apollo/client'
+import {setContext} from '@apollo/client/link/context'
+
+export const createApolloClient = (
+  getToken: () => string | undefined,
+): ApolloClient<NormalizedCacheObject> => {
+  const authLink = setContext(async (_, {headers}) => {
+    const token = getToken()
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
+
+  const httpLink = new HttpLink({
+    uri: process.env.REACT_APP_API_URL,
+  })
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      mutate: {
+        errorPolicy: 'all',
+      },
+    },
+  })
+}
