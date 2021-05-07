4b128d1f718bd6c2d1aa1817b758c49afc16aa1a
ansunrisein <ansunrisein@gmail.com>
2021-05-07 20:10:54 +0300

    Add user and loading to AuthContext

--- src/features/shared/contexts/AuthContext/providers/FirebaseAuthProvider/FirebaseAuthProvider.tsx
+++ src/features/shared/contexts/AuthContext/providers/FirebaseAuthProvider/FirebaseAuthProvider.tsx
@@ -1,28 +1,49 @@
-import React, {useCallback, useMemo} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 import Firebase from 'firebase/app'
+import {useAsync} from 'react-use'
+import {firebase} from '@eh/react/configs/firebase'
 import {AuthContext} from '../../AuthContext'
 
 export type FirebaseAuthProviderProps =
   | {
       firebaseConfig: Record<string, string>
+      firebaseApp?: undefined
     }
-  | {firebaseApp: Firebase.app.App}
+  | {
+      firebaseApp: Firebase.app.App
+      firebaseConfig?: undefined
+    }
 
 export const FirebaseAuthProvider: React.FC<FirebaseAuthProviderProps> = ({children, ...props}) => {
   const app = useMemo(() => {
-    if ('firebaseApp' in props) {
-      return props.firebaseApp
-    } else {
-      return Firebase.initializeApp(props.firebaseConfig)
+    const app = props.firebaseApp || Firebase.initializeApp(props.firebaseConfig)
+
+    if (process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_HOST) {
+      app.auth().useEmulator(process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_HOST)
     }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    return app
   }, [props.firebaseApp, props.firebaseConfig])
 
-  const login = useCallback(async () => {
-    await app.auth().signInWithPopup(new Firebase.auth.GoogleAuthProvider())
+  const [user, setUser] = useState<{token: string} | undefined>()
+
+  const {loading} = useAsync(async () => {
+    await new Promise(resolve => firebase.auth().onAuthStateChanged(resolve))
+
+    const user = app.auth().currentUser
+
+    if (user) {
+      return user.getIdToken(true).then(token => setUser({token}))
+    }
   }, [app])
 
-  return <AuthContext.Provider value={{login}}>{children}</AuthContext.Provider>
+  const login = useCallback(async () => {
+    await app
+      .auth()
+      .signInWithPopup(new Firebase.auth.GoogleAuthProvider())
+      .then(({user}) => user?.getIdToken(true))
+      .then(token => token && setUser({token}))
+  }, [app])
+
+  return <AuthContext.Provider value={{login, user, loading}}>{children}</AuthContext.Provider>
 }
