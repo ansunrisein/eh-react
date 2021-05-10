57710707f82b128132fa8f64ac58b0d96adcce69
ansunrisein <ansunrisein@gmail.com>
2021-05-10 21:52:21 +0300

    Replace MockedProvider with mock-apollo-client

--- src/features/horizon/pages/BoardPage/BoardPage.stories.tsx
+++ src/features/horizon/pages/BoardPage/BoardPage.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Meta, Story} from '@storybook/react'
 import {MemoryRouter, Route} from 'react-router-dom'
-import {MockedProvider} from '@apollo/client/testing'
-import {InMemoryCache} from '@apollo/client'
+import {ApolloProvider} from '@apollo/client'
+import {createMockClient} from 'mock-apollo-client'
+import delay from 'delay'
 import {board} from '../../views/Board/testData'
 import {BOARD} from '../../graphql'
 import {BoardPage} from './BoardPage'
@@ -13,23 +14,18 @@ export default {
   parameters: {layout: 'fullscreen', controls: {hideNoControlsWarning: true}},
   decorators: [
     Story => (
-      <MockedProvider
-        mocks={[
-          {
-            request: {query: BOARD, variables: {id: 'board1'}},
-            result: {data: {board}},
-          },
-        ]}
-        cache={new InMemoryCache({possibleTypes: {Event: ['TextEvent']}})}
-      >
+      <ApolloProvider client={client}>
         <MemoryRouter initialEntries={[{pathname: '/board1'}]}>
           <Route path="/:id">
             <Story />
           </Route>
         </MemoryRouter>
-      </MockedProvider>
+      </ApolloProvider>
     ),
   ],
 } as Meta
+
+const client = createMockClient()
+client.setRequestHandler(BOARD, () => delay(1000).then(() => ({data: {board: board}})))
 
 export const Usual: Story = () => <BoardPage />
