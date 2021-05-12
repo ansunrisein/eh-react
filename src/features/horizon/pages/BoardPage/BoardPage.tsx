09457b208924ced6e593a5b0aa586f0fd3316dcc
ansunrisein <ansunrisein@gmail.com>
2021-05-12 11:50:17 +0300

    Implement updating 'favorite' and 'pinned' board fields

--- src/features/horizon/pages/BoardPage/BoardPage.tsx
+++ src/features/horizon/pages/BoardPage/BoardPage.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import {Loader} from 'rsuite'
 import {useParams} from 'react-router-dom'
 import {useModal} from '@eh/react/features/shared/contexts/ModalContext'
 import {EventFormModal} from '@eh/react/features/event/modals'
 import {PageTemplate} from '@eh/react/features/shared/templates'
-import {useBoard} from '../../hooks'
+import {UpdateBoardVariables} from '@eh/react/features/horizon/graphql/types/UpdateBoard'
+import {useBoard, useUpdateBoard} from '../../hooks'
 import {Board} from '../../views'
 
 export const BoardPage: React.FC = () => {
   const {id} = useParams<{id: string}>()
 
-  const {open} = useModal(EventFormModal)
   const {board, loading} = useBoard({_id: id})
+  // TODO: loading for change pin and fav states
+  const {update} = useUpdateBoard()
+
+  const {open} = useModal(EventFormModal)
+
+  const onFavClick = useCallback(
+    (newBoard: UpdateBoardVariables) => update({...(board as any), favorite: !newBoard.favorite}),
+    [board, update],
+  )
+
+  const onPinClick = useCallback(
+    (newBoard: UpdateBoardVariables) => update({...(board as any), pinned: !newBoard.pinned}),
+    [board, update],
+  )
 
   if (!board || loading) {
     return <Loader center size="lg" />
@@ -19,7 +33,12 @@ export const BoardPage: React.FC = () => {
 
   return (
     <PageTemplate>
-      <Board board={board} onCreateEventClick={() => open({boardId: id})} />
+      <Board
+        board={board}
+        onCreateEventClick={() => open({boardId: id})}
+        onBoardFavClick={onFavClick}
+        onBoardPinClick={onPinClick}
+      />
     </PageTemplate>
   )
 }
--- src/features/horizon/pages/Horizon/Horizon.tsx
+++ src/features/horizon/pages/Horizon/Horizon.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import {useModal} from '@eh/react/features/shared/contexts/ModalContext'
 import {PageTemplate} from '@eh/react/features/shared/templates'
-import {useDashboard, useFilter, useSort} from '../../hooks'
+import {useDashboard, useFilter, useSort, useUpdateBoard} from '../../hooks'
 import {Dashboard} from '../../views'
 import {BoardFormModal} from '../../modals'
 import {filterConfig} from './filters'
@@ -14,6 +14,25 @@ export const Horizon: React.FC = () => {
   const {sortsState, setSortsState} = useSort(sortConfig)
 
   const {dashboard, loading} = useDashboard({filter: filtersState, sort: sortsState})
+  const {update} = useUpdateBoard()
+
+  const onFavClick = useCallback(
+    board =>
+      update({
+        ...(board as any),
+        favorite: !board.favorite,
+      }),
+    [update],
+  )
+
+  const onPinClick = useCallback(
+    board =>
+      update({
+        ...(board as any),
+        pinned: !board.pinned,
+      }),
+    [update],
+  )
 
   return (
     <PageTemplate>
@@ -21,10 +40,12 @@ export const Horizon: React.FC = () => {
         filters={filterConfig}
         sorts={sortConfig}
         boards={dashboard}
+        loading={loading}
         onFiltersChange={setFiltersState}
         onSortsChange={setSortsState}
         onCreateBoardClick={open}
-        loading={loading}
+        onBoardFavClick={onFavClick}
+        onBoardPinClick={onPinClick}
       />
     </PageTemplate>
   )
