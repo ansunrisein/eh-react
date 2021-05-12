a62f7b5c45e7d82d0cedc83eb945b2b430492422
ansunrisein <ansunrisein@gmail.com>
2021-05-12 11:58:54 +0300

    Provide user

--- src/features/shared/templates/PageTemplate/PageTemplate.tsx
+++ src/features/shared/templates/PageTemplate/PageTemplate.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Box, Flex} from 'reflexbox'
 import {Header} from '@eh/react/features/common/components'
+import {useAuth} from '@eh/react/features/shared/contexts/AuthContext'
 
-export const PageTemplate: React.FC = ({children}) => (
-  <Flex minHeight="100vh" flexDirection="column" padding="0 0.5rem 0.5rem">
-    <Header />
-    <Flex flexGrow={1}>
-      <Box as="main" width="100%">
-        {children}
-      </Box>
+export const PageTemplate: React.FC = ({children}) => {
+  const {user} = useAuth()
+
+  return (
+    <Flex minHeight="100vh" flexDirection="column" padding="0 0.5rem 0.5rem">
+      <Header isAuthenticated={!!user} />
+      <Flex flexGrow={1}>
+        <Box as="main" width="100%">
+          {children}
+        </Box>
+      </Flex>
     </Flex>
-  </Flex>
-)
+  )
+}
