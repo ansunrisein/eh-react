30c57602d207ebfaf22eab5d37da3de5649fe24b
ansunrisein <ansunrisein@gmail.com>
2021-06-15 19:35:49 +0300

    Update cache board

--- src/features/horizon/hooks/useCreateBoard/useCreateBoard.ts
+++ src/features/horizon/hooks/useCreateBoard/useCreateBoard.ts
@@ -1,5 +1,6 @@
 import {useCallback} from 'react'
 import {ApolloError, FetchResult, MutationHookOptions, useMutation} from '@apollo/client'
+import noop from 'noop6'
 import {CREATE_BOARD} from '../../graphql'
 import {CreateBoard, CreateBoardVariables} from '../../graphql/types/CreateBoard'
 
@@ -12,7 +13,12 @@ export type UseCreateBoardResult = {
 export const useCreateBoard = (
   options?: MutationHookOptions<CreateBoard, CreateBoardVariables>,
 ): UseCreateBoardResult => {
-  const [mutate, {loading, error}] = useMutation(CREATE_BOARD, options)
+  const [mutate, {loading, error}] = useMutation(CREATE_BOARD, {
+    ...options,
+    update: cache => {
+      cache.modify({fields: noop})
+    },
+  })
 
   const create = useCallback((variables: CreateBoardVariables) => mutate({variables}), [mutate])
 
