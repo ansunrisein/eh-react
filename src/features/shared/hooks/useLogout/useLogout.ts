69d7b22e28cc4467cadb40d74f31af45f4f2ad9a
ansunrisein <ansunrisein@gmail.com>
2021-06-20 22:08:50 +0300

    Redirect to id page after log out

--- src/features/shared/hooks/useLogout/useLogout.ts
+++ src/features/shared/hooks/useLogout/useLogout.ts
@@ -1,13 +1,16 @@
-import {useApolloClient} from '@apollo/client'
 import {useCallback} from 'react'
+import {useApolloClient} from '@apollo/client'
+import {useHistory} from 'react-router-dom'
 import {useAuth} from '../../contexts/AuthContext'
 
 export const useLogout = (): (() => Promise<void>) => {
   const apolloClient = useApolloClient()
   const {logout} = useAuth()
+  const history = useHistory()
 
   return useCallback(async () => {
     await logout()
     await apolloClient.cache.reset()
-  }, [logout, apolloClient])
+    history.push('/id')
+  }, [logout, apolloClient.cache, history])
 }
