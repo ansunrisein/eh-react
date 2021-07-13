9142d73e7e2749451c0a23cd66afbab808daaa31
ansunrisein <ansunrisein@gmail.com>
2021-07-13 18:48:22 +0300

    Install normalize.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type {AppProps} from 'next/app'
 import '../../index.css'
+import 'normalize.css'
 
 const MyApp = ({Component, pageProps}: AppProps) => {
   return <Component {...pageProps} />
