5b83e9f69dcba57439f6056d6e3c52b3de4daab4
ansunrisein <ansunrisein@gmail.com>
2021-07-26 01:02:14 +0300

    Change format stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const mergeBabelConfigs = require('babel-merge')
 const config = require('../.babelrc')
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/stories.mdx', '../src/**/stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   babel: options => mergeBabelConfigs(options, config),
   webpackFinal: async config => {
