cdfd9e98e88199e3abdc1d8369f9448a96010b5b
ansunrisein <ansunrisein@gmail.com>
2021-09-27 16:51:35 +0300

    Fix body-class-name lib

--- src/shared/lib/body-class-name/useBodyClassName.ts
+++ src/shared/lib/body-class-name/useBodyClassName.ts
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 
 export const useBodyClassName = (className: string): void => {
   useEffect(() => {
-    document.body.className = className
+    document.body.classList.add(className)
+    return () => document.body.classList.remove(className)
   }, [className])
 }
