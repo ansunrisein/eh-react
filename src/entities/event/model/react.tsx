88229f32fde1221ae7dac56619b87032018eee86
ansunrisein <ansunrisein@gmail.com>
2021-09-27 16:51:00 +0300

    Bind event model with react

---
+++ src/entities/event/model/react.tsx
@@ -0,0 +1,20 @@
+import React, {createContext, useContext} from 'react'
+import {EventEntity} from './event'
+
+export const EventEntityContext = createContext<EventEntity>(
+  new Proxy({} as EventEntity, {
+    get() {
+      throw new Error('Use EventEntityProvider!')
+    },
+  }),
+)
+
+export type EventEntityProviderProps = {
+  event: EventEntity
+}
+
+export const EventEntityProvider: React.FC<EventEntityProviderProps> = ({children, event}) => (
+  <EventEntityContext.Provider value={event}>{children}</EventEntityContext.Provider>
+)
+
+export const useEventEntity = (): EventEntity => useContext(EventEntityContext)
