cb90d0ead42155bafbfb7a8b7996cd152d4bbed8
ansunrisein <ansunrisein@gmail.com>
2021-10-04 12:19:22 +0300

    Implement Header

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Header} from './ui'
 import S from './Layout.module.scss'
 
-export const Layout: React.FC = ({children}) => (
+export type LayoutProps = {
+  header?: boolean
+}
+
+export const Layout: React.FC<LayoutProps> = ({header, children}) => (
   <div className={S.wrapper}>
+    {header && <Header />}
     <div>{children}</div>
   </div>
 )
