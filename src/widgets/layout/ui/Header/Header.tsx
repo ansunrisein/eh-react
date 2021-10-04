54af0e45179837764fd2b933e5c755da1eb03d45
ansunrisein <ansunrisein@gmail.com>
2021-10-04 12:19:22 +0300

    Implement Header

---
+++ src/widgets/layout/ui/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from 'rsuite'
+import {UserInfo, Dashboard} from '@rsuite/icons'
+import {Icon, Logo} from '../../../../shared/ui'
+import S from './Header.module.scss'
+
+export const Header: React.FC = () => (
+  <header className={S.header}>
+    <Button>
+      <Dashboard />
+    </Button>
+    <Logo />
+    <Icon icon={UserInfo} />
+  </header>
+)
