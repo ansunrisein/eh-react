726a9f2ee80ad13bae986c6437612d81e679071e
ansunrisein <ansunrisein@gmail.com>
2021-10-15 22:15:53 +0300

    Change Header

--- src/widgets/layout/ui/Header/Header.tsx
+++ src/widgets/layout/ui/Header/Header.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import {Button} from 'rsuite'
-import {UserInfo, Dashboard} from '@rsuite/icons'
-import {Icon, Logo} from '@eh/shared/ui'
+import {IconButton} from 'rsuite'
+import {Dashboard, UserInfo} from '@rsuite/icons'
+import {Logo} from '@eh/shared/ui'
 import S from './Header.module.scss'
 
 export const Header: React.FC = () => (
   <header className={S.header}>
-    <Button>
-      <Dashboard />
-    </Button>
+    <IconButton icon={<Dashboard />} size="md" appearance="link" className={S.link} />
     <Logo />
-    <Icon icon={UserInfo} />
+    <IconButton icon={<UserInfo />} size="md" appearance="link" className={S.link} />
   </header>
 )
