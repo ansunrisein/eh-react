361a2a3184c89e3035e3992ea4c8967f6d63da55
ansunrisein <ansunrisein@gmail.com>
2021-10-17 22:56:58 +0300

    Implement reflexbox

---
+++ src/shared/lib/reflexbox/Flex.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+type DefaultType = 'initial' | 'inherit'
+
+export type FlexType = {
+  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse' | DefaultType
+  flexWrap?: 'nowrap' | 'wrap' | 'wrap-reverse' | DefaultType
+  order?: number
+  flex?: number | 'auto' | 'none' | DefaultType
+  flexGrow?: number | DefaultType
+  flexShrink?: number | DefaultType
+  flexBasis?: 'auto' | 'content' | string | number | DefaultType
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | DefaultType
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch' | DefaultType
+  alignSelf?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch' | DefaultType
+  alignContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'stretch'
+    | DefaultType
+}
+
+export type CommonType = {
+  width?: string | number
+  height?: string | number
+}
+
+export const Flex: React.FC<FlexType & CommonType & React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  flexDirection,
+  flexWrap,
+  order,
+  flex,
+  flexGrow,
+  flexShrink,
+  flexBasis,
+  justifyContent,
+  alignItems,
+  alignSelf,
+  alignContent,
+  style = {},
+  ...props
+}) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection,
+      flexWrap,
+      order,
+      flex,
+      flexGrow,
+      flexShrink,
+      flexBasis,
+      justifyContent,
+      alignItems,
+      alignSelf,
+      alignContent,
+      ...style,
+    }}
+    {...props}
+  >
+    {children}
+  </div>
+)
