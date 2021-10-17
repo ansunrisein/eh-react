001e9540b3b46383939d5e1f5e28ffefda30c555
ansunrisein <ansunrisein@gmail.com>
2021-10-17 23:01:59 +0300

    Implement switch and persist theme

--- src/shared/lib/theme/ThemeContext.tsx
+++ src/shared/lib/theme/ThemeContext.tsx
@@ -1,7 +1,13 @@
-import React, {createContext, useCallback, useContext, useState} from 'react'
+import React, {createContext, useCallback, useContext} from 'react'
 import noop from '@stdlib/utils-noop'
+import {useMedia} from 'react-use'
+import {usePersistedState} from '@eh/shared/lib/use-persisted-state'
 
-export type Theme = 'dark' | 'light'
+export enum ThemeEnum {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
+export type Theme = Lowercase<keyof typeof ThemeEnum>
 
 export type ThemeContextType = {
   theme: Theme
@@ -15,11 +21,24 @@ export type ThemeProviderProps = {
 export const ThemeContext = createContext<ThemeContextType>({theme: 'light', toggle: noop})
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({defaultTheme, children}) => {
-  const [theme, setTheme] = useState(defaultTheme)
+  const isDarkThemePreferred = useMedia('(prefers-color-scheme: dark)')
+  const [theme, setTheme] = usePersistedState<Theme>('theme', value => {
+    if (!value && isDarkThemePreferred) {
+      return 'dark'
+    }
 
-  const toggle = useCallback(() => setTheme(theme => (theme === 'dark' ? 'light' : 'dark')), [])
+    return isTheme(value) ? value : defaultTheme
+  })
+
+  const toggle = useCallback(
+    () => setTheme(theme => (theme === 'dark' ? 'light' : 'dark')),
+    [setTheme],
+  )
 
   return <ThemeContext.Provider value={{theme, toggle}}>{children}</ThemeContext.Provider>
 }
 
 export const useTheme = (): ThemeContextType => useContext(ThemeContext)
+
+export const isTheme = (value: unknown): value is Theme =>
+  typeof value === 'string' && value.toUpperCase() in ThemeEnum
