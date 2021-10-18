432bb83d575270ac1e60ef89256e8306876f3eda
ansunrisein <ansunrisein@gmail.com>
2021-10-18 13:05:44 +0300

    Replace rsuite icons with Remix Icon

--- src/widgets/layout/ui/header/Header.tsx
+++ src/widgets/layout/ui/header/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import {IconButton} from 'rsuite'
-import {Dashboard} from '@rsuite/icons'
+import {Icon} from '@rsuite/icons'
+import {RiBarChartBoxFill} from 'react-icons/ri'
 import {Logo} from '@eh/shared/ui'
 import {ThemeSwitcher} from '../theme-switcher'
 import S from './Header.module.scss'
 
 export const Header: React.FC = () => (
   <header className={S.header}>
-    <IconButton icon={<Dashboard />} size="md" appearance="link" className={S.link} />
+    <IconButton
+      icon={<Icon as={RiBarChartBoxFill} />}
+      size="md"
+      appearance="link"
+      className={S.link}
+    />
     <Logo />
     <ThemeSwitcher size="md" appearance="link" className={S.link} />
   </header>
