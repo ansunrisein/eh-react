bd3ce865bfec8fe9161d7cc2ee9d3e698fd097ed
ansunrisein <ansunrisein@gmail.com>
2021-10-20 13:27:29 +0300

    Fix empty component

--- src/shared/ui/empty/Empty.tsx
+++ src/shared/ui/empty/Empty.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import c from 'classnames'
 import {Flex} from '@eh/shared/lib/reflexbox'
+import S from './Empty.module.scss'
 
-export const Empty: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => (
-  <Flex alignItems="center" justifyContent="center" flexGrow={1} {...props}>
+export const Empty: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <Flex
+    alignItems="center"
+    justifyContent="center"
+    flexGrow={1}
+    className={c(S.empty, className)}
+    {...props}
+  >
     {children}
   </Flex>
 )
