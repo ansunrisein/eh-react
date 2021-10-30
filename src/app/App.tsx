811a5be7695546e3870986da505d63712355caa4
ansunrisein <ansunrisein@gmail.com>
2021-10-30 21:28:38 +0300

    Suspense app render while firebase is initializing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {Loader} from 'rsuite'
+import {FirebaseAuthSuspense} from '@eh/features/auth-with-firebase'
 import {Board} from '../pages/board'
-import {AppStoreProvider} from './store'
+import {auth} from './config/firebase'
+import {AppStoreProvider} from './config/store'
 import {AppThemeProvider} from './theme'
 
 export const App: React.FC = () => (
   <AppStoreProvider>
     <AppThemeProvider>
-      <Board />
+      <FirebaseAuthSuspense auth={auth} fallback={<Loader />}>
+        <Board />
+      </FirebaseAuthSuspense>
     </AppThemeProvider>
   </AppStoreProvider>
 )
