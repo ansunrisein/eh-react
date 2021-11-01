8ea80ea37ec39ef644374187a3cff49280835a44
ansunrisein <ansunrisein@gmail.com>
2021-11-01 19:59:17 +0300

    Decorate board story

--- src/pages/board/Board.stories.tsx
+++ src/pages/board/Board.stories.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import {Meta, Story} from '@storybook/react'
 import {createDomain} from 'effector'
-import {createEventEntity, EventEntityProvider} from '@eh/entities/event/model'
+import {hocsToDecorators} from '@eh/shared/lib/hocs-to-decorators'
+import {authMock} from '@eh/shared/mocks/firebase'
+import {createSessionEntity, withSessionEntity} from '@eh/entities/session'
+import {createEventEntity, withEventEntity} from '@eh/entities/event'
+import {
+  createAuthWithFirebaseFeature,
+  withAuthWithFirebaseFeature,
+} from '@eh/features/auth-with-firebase'
 import {Board} from './Board'
+
+const domain = createDomain()
+const sessionEntity = createSessionEntity({domain})
+const eventEntity = createEventEntity({domain})
+const authWithFirebaseFeature = createAuthWithFirebaseFeature({
+  auth: authMock,
+  session: sessionEntity,
+})
 
 export default {
   title: 'pages/board',
   component: Board,
   parameters: {docs: {source: {type: 'code'}}, layout: 'fullscreen'},
-  decorators: [
-    Story => (
-      <EventEntityProvider event={createEventEntity({domain: createDomain()})}>
-        <Story />
-      </EventEntityProvider>
-    ),
-  ],
+  decorators: hocsToDecorators([
+    withSessionEntity({session: sessionEntity}),
+    withEventEntity({event: eventEntity}),
+    withAuthWithFirebaseFeature({authWithFirebase: authWithFirebaseFeature}),
+  ]),
 } as Meta
 
 export const Default: Story = props => <Board {...props} />
