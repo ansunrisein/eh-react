49d4035d821853e693e277289c905a3ee133ca0f
ansunrisein <ansunrisein@gmail.com>
2021-11-06 23:39:35 +0300

    Add Layout 'loading' prop

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import {Loader} from 'rsuite'
 import {Header} from './ui'
 import S from './Layout.module.scss'
 
 export type LayoutProps = {
   header?: boolean
+  loading?: boolean
 }
 
-export const Layout: React.FC<LayoutProps> = ({header, children}) => (
+export const Layout: React.FC<LayoutProps> = ({header, loading, children}) => (
   <div className={S.wrapper}>
     {header && <Header />}
-    <div className={S.children}>{children}</div>
+    <div className={S.children}>
+      {loading ? <Loader backdrop center size="lg" /> : <>{children}</>}
+    </div>
   </div>
 )
