b9a93aa8cd4e880c5642bebf062d849d49d22743
ansunrisein <ansunrisein@gmail.com>
2021-11-06 23:26:30 +0300

    Configure apollo

---
+++ src/app/config/apollo.ts
@@ -0,0 +1,23 @@
+import {ApolloClient, concat, HttpLink, InMemoryCache} from '@apollo/client'
+import {setContext} from '@apollo/client/link/context'
+import {sessionEntity} from './store'
+
+const authLink = setContext((_, {headers}) => {
+  const token = sessionEntity.$token.getState()
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  }
+})
+
+const httpLink = new HttpLink({
+  uri: `${process.env.REACT_APP_API_URL}/graphql`,
+})
+
+export const apollo = new ApolloClient({
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache(),
+})
