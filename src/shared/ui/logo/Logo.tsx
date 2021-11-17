22d0c5d56478467cf8bab479389eba312322c2ab
ansunrisein <ansunrisein@gmail.com>
2021-11-17 22:48:53 +0300

    Improve logo responsiveness

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -4,8 +4,10 @@ import S from './Logo.module.scss'
 
 export type LogoProps = {
   size?: 'lg' | 'md'
-}
+} & React.HTMLAttributes<HTMLHeadingElement>
 
-export const Logo: React.FC<LogoProps> = ({size = 'md'}) => (
-  <h4 className={cx(S.logo, S[`size-${size}`])}>Event Horizon</h4>
+export const Logo: React.FC<LogoProps> = ({size = 'md', className, ...props}) => (
+  <h4 className={cx(S.logo, S[`size-${size}`], className)} {...props}>
+    Event Horizon
+  </h4>
 )
