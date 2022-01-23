ee04782f4da5eddb7bd8133229bc288edb9d0b51
ansunrisein <ansunrisein@gmail.com>
2022-01-23 22:16:44 +0300

    Implement created events ui

--- src/entities/event/model/event.ts
+++ src/entities/event/model/event.ts
@@ -1,6 +1,6 @@
 import {Domain} from 'effector'
 import {ApolloClient} from '@apollo/client'
-import {Board, ConnectionRef, createEmptyConnection, EventConnection} from '@eh/shared/api'
+import {isDefined} from '@eh/shared/lib/is-defined'
 import {
   CreateEventDocument,
   CreateEventMutation,
@@ -8,7 +8,7 @@ import {
   EditEventDocument,
   EditEventMutation,
   EditEventMutationVariables,
-  EventFragmentDoc,
+  EventFragment,
   RemoveEventDocument,
   RemoveEventMutation,
   RemoveEventMutationVariables,
@@ -27,33 +27,6 @@ export const createEventEntity = ({domain, apollo}: EventEntityDeps) => {
       .mutate<CreateEventMutation, CreateEventMutationVariables>({
         mutation: CreateEventDocument,
         variables,
-        update: (cache, {data}) => {
-          cache.modify({
-            id: cache.identify({__typename: 'Board', _id: variables.boardId} as Pick<
-              Board,
-              '__typename' | '_id'
-            >),
-            fields: {
-              events: (
-                prevEvents: ConnectionRef<EventConnection> = createEmptyConnection('Event'),
-              ): ConnectionRef<EventConnection> =>
-                data?.createEvent
-                  ? {
-                      ...prevEvents,
-                      edges: prevEvents.edges.concat({
-                        __typename: 'EventEdge',
-                        cursor: data.createEvent._id,
-                        node: cache.writeFragment({
-                          id: `${data.createEvent.__typename}:${data.createEvent._id}`,
-                          data: data.createEvent,
-                          fragment: EventFragmentDoc,
-                        }),
-                      }),
-                    }
-                  : prevEvents,
-            },
-          })
-        },
       })
       .then(result => result?.data?.createEvent),
   )
@@ -80,9 +53,21 @@ export const createEventEntity = ({domain, apollo}: EventEntityDeps) => {
       .then(result => result?.data?.removeEvent),
   )
 
+  const resetNewEvents = domain.event()
+
+  const $newEvents = domain
+    .store<EventFragment[]>([])
+    .on(createEventFx.doneData.filter({fn: isDefined}), (events, newEvent) => [newEvent, ...events])
+    .on(removeEventFx.doneData.filter({fn: isDefined}), (events, removedEvent) =>
+      events.filter(event => event._id !== removedEvent._id),
+    )
+    .reset(resetNewEvents)
+
   return {
     createEventFx,
     editEventFx,
     removeEventFx,
+    resetNewEvents,
+    $newEvents,
   }
 }
--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,12 +1,14 @@
 import React, {useCallback, useState} from 'react'
+import {RiHashtag} from 'react-icons/ri'
 import {useMedia, useTitle} from 'react-use'
-import {Button, Drawer} from 'rsuite'
+import {Button, Divider, Drawer} from 'rsuite'
 import {useBooleanState} from 'use-boolean-state'
+import {Icon} from '@rsuite/icons'
 import {Flex} from '@eh/shared/lib/reflexbox'
 import {useNavigate, useParams} from '@eh/shared/lib/router'
 import {Empty, Modal} from '@eh/shared/ui'
 import {usePermissions} from '@eh/entities/board'
-import {EventCard} from '@eh/entities/event'
+import {EventCard, useNewEvents, useNewEventsGate} from '@eh/entities/event'
 import {CreateEventForm} from '@eh/features/update-event'
 import {BoardSettings} from '@eh/widgets/board-settings'
 import {Layout} from '@eh/widgets/layout'
@@ -25,6 +27,8 @@ export const Board: React.FC = () => {
   const {board, loading} = useFullBoard(id)
   const {canCreateEvent, canUpdateEvent, canRemoveEvent, canViewSettings} = usePermissions(board)
 
+  const newEvents = useNewEvents()
+
   useTitle(`Board | ${board?.title || ''}`)
 
   const remove = useCallback(() => {
@@ -33,6 +37,8 @@ export const Board: React.FC = () => {
   }, [closeBoardSettings, navigate])
 
   const isTablet = useMedia('(min-width: 768px)')
+
+  useNewEventsGate()
 
   return (
     <Layout header loading={loading}>
@@ -54,15 +60,7 @@ export const Board: React.FC = () => {
         </Flex>
       )}
 
-      {board?.events.edges.length ? (
-        <ul className={S.grid}>
-          {board.events.edges.map(e => (
-            <li key={e.node._id} onClick={() => setOpenedEventId(e.node._id)}>
-              <EventCard event={e.node} className={S.event} />
-            </li>
-          ))}
-        </ul>
-      ) : (
+      {!newEvents.length && !board?.events.edges.length ? (
         <Empty>
           <p>There is no events in this board :(</p>
           {canCreateEvent && (
@@ -71,6 +69,35 @@ export const Board: React.FC = () => {
             </Button>
           )}
         </Empty>
+      ) : (
+        <>
+          {!!newEvents.length && (
+            <div>
+              <h4 className={S.title}>
+                <Icon as={RiHashtag} />
+                <span className={S.vertical}>Latest created events</span>
+              </h4>
+              <ul className={S.grid}>
+                {newEvents.map(e => (
+                  <li key={e._id} onClick={() => setOpenedEventId(e._id)}>
+                    <EventCard event={e} className={S.event} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {!!newEvents.length && !!board?.events.edges && <Divider />}
+
+          <ul className={S.grid}>
+            {!!board?.events.edges &&
+              board.events.edges.map(e => (
+                <li key={e.node._id} onClick={() => setOpenedEventId(e.node._id)}>
+                  <EventCard event={e.node} className={S.event} />
+                </li>
+              ))}
+          </ul>
+        </>
       )}
 
       <Modal open={openedEventId !== null} onClose={() => setOpenedEventId(null)} backdrop>
