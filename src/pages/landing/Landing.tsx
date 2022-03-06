a78f951f64d80847ba99c010e0b85e83b85f0de9
ansunrisein <ansunrisein@gmail.com>
2022-03-06 21:27:37 +0300

    Add LocaleSwitcher to Landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useMedia, useTitle} from 'react-use'
+import {LocaleSwitcher} from '@eh/shared/lib/i18n'
+import {Flex} from '@eh/shared/lib/reflexbox'
 import {ThemeSwitcher, useTheme} from '@eh/shared/lib/theme'
 import {Logo} from '@eh/shared/ui'
 import {LoginButton} from '@eh/features/auth-with-firebase'
@@ -18,7 +20,10 @@ export const Landing: React.FC = () => {
     <div className={S[theme]}>
       <Layout>
         <div className={S.landing}>
-          <ThemeSwitcher color={color} className={S.switcher} appearance="link" />
+          <Flex alignItems="center" alignSelf="flex-end" gap="10px">
+            <LocaleSwitcher color={color} />
+            <ThemeSwitcher color={color} appearance="link" />
+          </Flex>
 
           <Logo size="lg" className={S.logo} />
 
