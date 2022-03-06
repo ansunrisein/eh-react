beb53d78eae4e41d1756ddc67a9018836f6424eb
ansunrisein <ansunrisein@gmail.com>
2022-03-06 21:27:10 +0300

    Extract LocaleSwitcher

--- src/shared/lib/i18n/index.ts
+++ src/shared/lib/i18n/index.ts
@@ -1,2 +1,3 @@
+export * from './ui'
 export * from './i18n'
 export * from './react'
--- src/shared/lib/i18n/ui/locale-switcher/LocaleSwitcher.tsx
+++ src/shared/lib/i18n/ui/locale-switcher/LocaleSwitcher.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import cx from 'classnames'
 import {RiEarthFill} from 'react-icons/ri'
-import {Dropdown, IconButton} from 'rsuite'
+import {Dropdown, IconButton, IconButtonProps} from 'rsuite'
 import {Icon} from '@rsuite/icons'
 import {useLocale, useSetLocale, useSupportedLocales} from '@eh/shared/lib/i18n'
 import S from './LocaleSwitcher.module.scss'
 
-export const LocaleSwitcher: React.FC = () => {
+export const LocaleSwitcher: React.FC<IconButtonProps> = ({color, ...props}) => {
   const setLocale = useSetLocale()
   const locale = useLocale()
   const supportedLocales = useSupportedLocales()
 
   return (
     <Dropdown
-      renderToggle={(props, ref) => (
+      renderToggle={(toggleProps, ref) => (
         <IconButton
-          className={S.button}
+          className={cx(!color && S.button)}
           size="md"
           appearance="link"
           icon={<Icon as={RiEarthFill} />}
+          color={color}
           {...props}
+          {...toggleProps}
           ref={ref}
         />
       )}
