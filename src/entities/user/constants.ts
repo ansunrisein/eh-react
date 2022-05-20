558410c2cd4682c3f40619c03235486ceda268c2
ansunrisein <ansunrisein@gmail.com>
2022-05-20 13:01:23 +0300

    Extract default user avatar to user entity constants

---
+++ src/entities/user/constants.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_USER_AVATAR = 'https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png'
--- src/entities/user/ui/avatar/Avatar.tsx
+++ src/entities/user/ui/avatar/Avatar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Avatar as RAvatar, AvatarProps as RAvatarProps, Loader} from 'rsuite'
 import {Flex} from '@eh/shared/lib/reflexbox'
+import {DEFAULT_USER_AVATAR} from '../../constants'
 import {useUser} from '../../model'
 
 export type AvatarProps = RAvatarProps
@@ -13,10 +14,7 @@ export const Avatar: React.FC<AvatarProps> = props => {
       {loading ? (
         <Loader size="sm" center />
       ) : (
-        <RAvatar
-          src={user?.avatar || 'https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png'}
-          {...props}
-        />
+        <RAvatar src={user?.avatar || DEFAULT_USER_AVATAR} {...props} />
       )}
     </Flex>
   )
