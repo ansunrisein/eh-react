13ee05bef7d6c714b6181b400fccab76804b1497
ansunrisein <ansunrisein@gmail.com>
2022-06-04 13:55:55 +0300

    Fix translation

--- src/pages/world/ui/mini-board/texts.ts
+++ src/pages/world/ui/mini-board/texts.ts
@@ -2,11 +2,7 @@ import {defineMessages} from 'react-intl'
 
 export const texts = defineMessages({
   noEvents: {
-    id: 'pages.dashboard.mini-board.noEvents',
+    id: 'pages.world.mini-board.noEvents',
     defaultMessage: 'There is no events',
-  },
-  createEvent: {
-    id: 'pages.dashboard.mini-board.createEvent',
-    defaultMessage: '<link>Go to the board</link> to create event! :)',
   },
 })
